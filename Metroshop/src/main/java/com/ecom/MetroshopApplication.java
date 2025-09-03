ef4a45f2ceb5fd686ed1fe57ac3cac5620c3b6a5
satyampandeyhcc <satyampandeyhcc@gmail.com>
2025-09-04 02:24:12 +0530

    first commit

---
+++ Metroshop/src/main/java/com/ecom/MetroshopApplication.java
@@ -0,0 +1,13 @@
+package com.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroshopApplication.class, args);
+	}
+
+}
